a55bbb20ff53423f8cb235df1b4b2f09f1804fae
rkskek8484-cell <rkskek8484@gmail.com>
2026-06-24 14:13:57 +0900

    feat: update article update

--- src/components/UpdateArticle.jsx
+++ src/components/UpdateArticle.jsx
@@ -1,22 +1,33 @@
-function UpdateArticle({ onSubmit }) {
+import { useState } from 'react';
+
+function UpdateArticle({ title, desc, onSubmit }) {
   console.log('UpdateArticle render');
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDesc, setNewDesc] = useState(desc);
+  const handleTitleChange = (e) => {
+    setNewTitle(e.target.value);
+  };
+  const handleDescChange = (e) => {
+    setNewDesc(e.target.value);
+  };
+
   return (
     <>
-      <h2>Create Article</h2>
+      <h2>Update Article</h2>
       <form
         action=''
         onSubmit={(e) => {
           e.preventDefault();
-          onSubmit(e.target.title.value, e.target.desc.value);
+          onSubmit(newTitle, newDesc);
         }}
       >
         <div>
           <label htmlFor='title'>title</label>
-          <input type='text' name='title' id='title' />
+          <input type='text' name='title' id='title' value={newTitle} onChange={handleTitleChange} />
         </div>
         <div>
           <label htmlFor='desc'>desc</label>
-          <textarea name='desc' id='desc'></textarea>
+          <textarea name='desc' id='desc' value={newDesc} onChange={handleDescChange}></textarea>
         </div>
         <button>Submit</button>
       </form>
