075b776efc8110056b9e2a3577af014826780d80
rkskek8484-cell <rkskek8484@gmail.com>
2026-06-24 16:39:21 +0900

    feat: create 내용 추가

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,18 @@ function App() {
     desc: '기본언어인 html, css, javascript부터 학습합니다.',
   });
   const [content, setContent] = useState([
-    { id: '1', title: 'UI/UX 개발', desc: '사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현', level: '난이도: 1' },
+    { id: '1', title: 'UI/UX 개발', desc: '사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현', level: '1' },
     {
       id: '2',
       title: '재사용이 가능한 UI 개발',
       desc: '컴포넌트 기반으로 동일한 UI를 효율적으로 재사용 가능',
-      level: '난이도: 2',
+      level: '2',
     },
     {
       id: '3',
       title: '애니메이션 구현',
       desc: '상태 변화에 따른 자연스럽고 동적인 화면 효과 구현',
-      level: '난이도: 5',
+      level: '5',
     },
   ]);
   // const [maxId, setMaxId] = useState(3);
@@ -75,10 +75,10 @@ function App() {
   } else if (mode === 'create') {
     _article = (
       <CreateArticle
-        onSubmit={(_title, _desc) => {
+        onSubmit={(_title, _desc, _level) => {
           const newId = uuidv4();
 
-          let _contents = content.concat({ id: newId, title: _title, desc: _desc });
+          let _contents = content.concat({ id: newId, title: _title, desc: _desc, level: _level });
           setContent(_contents);
           // setMaxId(newId);
           setId(newId);
